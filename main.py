1f46c2966994e17984a0384f1e5d8be2da046ed6
Lhianee <v-schulz@gmx.net>
2025-04-20 23:23:25 +0200

    Dynamische Dateipfade eingefügt.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+import os  # Operatind System
+
+# Dynamischer Dateipfad je nach Umgebung
+if os.name == "nt":  # Windows
+    dateipfad = r"E:\Onedrive\02Python\Github\Repositories\Bookbot\books\frankenstein.txt"
+else:  # Linux (WSL)
+    dateipfad = "/mnt/e/Onedrive/02Python/Github/Repositories/Bookbot/books/frankenstein.txt"
+
+# Funktion zur Verarbeitung des Buchinhalts
 def get_book_text(dateipfad):
-    with open("E:\Onedrive\02Python\Github\Repositories\Bookbot\books\frankenstein.txt", "r") as datei:
+    with open(dateipfad, encoding="utf-8") as datei:
         inhalt = datei.read()
         return inhalt
+
+# Hauptfunktion: Text holen und ausgeben
 def main():
-    import os
-    dateipfad = r"E:\Onedrive\02Python\Github\Repositories\Bookbot\books\frankenstein.txt"    
     if os.path.exists(dateipfad):
         buch_inhalt = get_book_text(dateipfad)
         print(buch_inhalt)
     else:
         print("Datei wurde nicht gefunden!")
-main ()
+
+# Skript starten
+main()
