e28d4b8940c3a7727e49d1853f8d493ddf9af7b5
Lhianee <v-schulz@gmx.net>
2025-04-20 22:31:27 +0200

    get_book_text implementiert

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(dateipfad):
+    with open(dateipfad, encoding="utf-8") as datei:
+        inhalt = datei.read()
+        return inhalt
+def main():
+    import os
+    dateipfad = r"E:\Onedrive\02 Python\Github\Repositories\Bookbot\books\frankenstein.txt"    
+    if os.path.exists(dateipfad):
+        buch_inhalt = get_book_text(dateipfad)
+        print(buch_inhalt)
+    else:
+        print("Datei wurde nicht gefunden!")
+main ()
